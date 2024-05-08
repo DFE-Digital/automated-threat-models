851356a3446aae31943a24c714f9f048ecf6a830
pritchyspritch <s.pritch@protonmail.com>
2024-05-08 16:49:27 +0100

    Add key vault and redis cache types

--- build-tm-from-azure-data.py
+++ build-tm-from-azure-data.py
@@ -32,6 +32,40 @@ def build_container_app_tm(name: str, asset_type: str) -> str:
     return container_app_asset_yaml
 
 
+def build_key_vault_tm(name: str, asset_type: str) -> str:
+    container_app_dict = {
+        "name": name,
+        "type": asset_type.split('/')[0],
+        "description": "A key vault used to hold sensitive keys, secrets, and config.",
+        "size": "service",
+        "technology": "vault",
+        "machine": "virtual"
+    }
+    template_file = open("technical_asset_template.yaml")
+    template_str = template_file.read()
+    tech_asset_template = Template(template_str)
+    container_app_asset_yaml = tech_asset_template.render(container_app_dict)
+
+    return container_app_asset_yaml
+
+
+def build_cache_tm(name: str, asset_type: str) -> str:
+    container_app_dict = {
+        "name": name,
+        "type": asset_type.split('/')[0],
+        "description": "A redis cache for holding data for reliability.",
+        "size": "service",
+        "technology": "database",
+        "machine": "virtual"
+    }
+    template_file = open("technical_asset_template.yaml")
+    template_str = template_file.read()
+    tech_asset_template = Template(template_str)
+    container_app_asset_yaml = tech_asset_template.render(container_app_dict)
+
+    return container_app_asset_yaml
+
+
 def produce_assets() -> list:
     assets_list = temp_file_read()
     yaml_list = []
@@ -44,7 +78,14 @@ def produce_assets() -> list:
             container_app_asset_yaml = build_container_app_tm(name, asset_type)
             yaml_list.append(container_app_asset_yaml)
             print(container_app_asset_yaml)
-    
+        elif asset_type == "microsoft.keyvault/vaults":
+            container_app_asset_yaml = build_key_vault_tm(name, asset_type)
+            yaml_list.append(container_app_asset_yaml)
+            print(container_app_asset_yaml)
+        elif asset_type == "microsoft.cache/redis":
+            container_app_asset_yaml = build_cache_tm(name, asset_type)
+            yaml_list.append(container_app_asset_yaml)
+            print(container_app_asset_yaml)
     return yaml_list
 
 
